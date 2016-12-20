b8800ecd8847cdde3dde288f26a162bff7da832e
Jeremy Yen <jeremy5189@gmail.com>
2016-12-20 14:44:08 +0800

    Add decode

--- routes/upload.js
+++ routes/upload.js
@@ -46,7 +46,7 @@ router.post('/encode', function(req, res) {
 
                     if(status) {
 
-                        console.info('Call Shell');
+                        console.info('Call Shell Encode');
 
                         shell( '/usr/bin/python', [
                             '/home/jeremy/BlindWaterMark/bwm.py',
@@ -56,7 +56,70 @@ router.post('/encode', function(req, res) {
                             '/home/jeremy/InvWatermark/public/photo/encoded_' + ts + '.png'
                         ], function(ret) {
                             console.log(ret);
-                            res.render('upload', {
+                            res.render('encode', {
+                                ts: ts
+                            });
+                        });
+                    }
+                }
+            });
+        }
+    });
+
+
+});
+
+router.post('/decode', function(req, res) {
+
+    var base_photo, watermark_photo, ret;
+
+    if (!req.files) {
+        res.send('No files were uploaded.');
+        return;
+    }
+
+    var ts = new Date().getTime();
+
+    base_photo = req.files.base_photo;
+    watermark_photo = req.files.watermark_photo;
+
+    console.info(base_photo);
+    console.info(watermark_photo);
+
+    var base_photo_new = '/tmp/base_photo_' + ts + '.png',
+        watermark_photo_new = '/tmp/watermark_photo_' + ts + '.png';
+
+    var status = true;
+
+    base_photo.mv(base_photo_new, function(err) {
+        if (err) {
+            status = false;
+            res.status(500).send(err);
+        }
+        else {
+            console.info('base_photo ok');
+
+            watermark_photo.mv(watermark_photo_new, function(err) {
+                if (err) {
+                    status = false;
+                    res.status(500).send(err);
+                }
+                else {
+                    console.info('watermark_photo ok');
+
+                    if(status) {
+
+                        console.info('Call Shell Decode');
+
+                        shell( '/usr/bin/python', [
+                            '/home/jeremy/BlindWaterMark/bwm.py',
+                            'decode',
+                            base_photo_new,
+                            watermark_photo_new,
+                            '/home/jeremy/InvWatermark/public/photo/decoded_' + ts + '.png'
+                        ], function(ret) {
+                            console.log(ret);
+                            res.render('decode', {
                                 ts: ts
                             });
                         });
